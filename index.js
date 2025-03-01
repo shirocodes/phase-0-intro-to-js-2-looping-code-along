7646418d22959ff659a03ec48f598cd867d04f97
shirocodes <ishirapt@gmail.com>
2025-03-01 14:03:36 +0300

    tackled with debugger, for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let cardMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        // debugger;
+        let cardMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        cardMessages.push(cardMessage);
+    }
+    return cardMessages;
+}
+
+const eventName = "surprise";
+const names = ["Guadalupe", "Ollie", "Aki"];
+writeCards(names, eventName);
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+
+
